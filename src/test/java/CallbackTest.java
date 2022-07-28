86f97335ad94edc6c4ddd0cec5b488dd12b3d06d
trukhachev495 <sergey.trainings@gmail.com>
2022-07-28 21:17:01 +0300

    passed

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -32,7 +32,7 @@ public class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
